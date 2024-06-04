80816a3258ccb111f387b4a22c07ad12aa791446
bharvey14 <bjh14@live.com>
2024-06-03 21:29:50 -0500

    Created base HTML template for active quiz and home page

--- quiz_home/views.py
+++ quiz_home/views.py
@@ -3,4 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def quiz_home(request):
-    return render(request, "/quiz_home.html")
+    pass
+
+def active_quiz(request):
+    pass
+
+def question():
+    pass
